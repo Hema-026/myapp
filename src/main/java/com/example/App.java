be279f7e46c80834d2bf68b8def7a30446479003
Hema-026 <hematr08@gmail.com>
2025-06-09 10:19:34 +0000

    first commit:myapp

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int a=10,b=2;
+        int sum=a+b;
+        System.out.println("Sum="+sum);
+    }
+}
